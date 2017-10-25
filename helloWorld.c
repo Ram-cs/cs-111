75a4b944512d70adb53f1feef9c4a78055e9ae65
ram-cs <ramusa2011@ucla.edu>
2017-10-24 17:13:59 -0700

    first commit

---
+++ helloWorld.c
@@ -0,0 +1,10 @@
+/***********************************
+ NAME: Ram Yadav
+ EMAIL: ramyadav4516@gmail.com
+ ID: 604841556
+ **********************************/
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+}
